68fab09f1e6a977454769804b44a8b9d472ee047
rizhima <arishermawan@hotmail.com>
2017-12-18 05:54:15 +0700

    delete model users & change gopay field name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171217072600) do
+ActiveRecord::Schema.define(version: 20171217225137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20171217072600) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.integer "gopay_credit_id"
+    t.integer "gopay_id"
   end
 
   create_table "drivers", force: :cascade do |t|
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 20171217072600) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.integer "status"
-    t.integer "gopay_credit_id"
+    t.integer "gopay_id"
     t.integer "location_id"
   end
 
